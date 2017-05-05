0dc3aeb111f2bbde4aa3aa3b179420f835bcd34f
James-Charlesworth <jcharlesworthwork@gmail.com>
2017-05-05 16:32:50 +0100

    submission, mid calculation stage 7

--- src/javaTest/Main.java
+++ src/javaTest/Main.java
@@ -25,17 +25,87 @@ public class Main {
 		ArrayList<String> marinas = reader1.sortArrays(all, "marinas");
 		ArrayList<String> boats = reader1.sortArrays(all, "boats");
 		ArrayList<String> people = reader1.sortArrays(all, "people");
-//		System.out.println(all);
-//		System.out.println(marinas);
-//		System.out.println(boats);
-//		System.out.println(people);
+
 		
 		ArrayList<Marina> marinaObjs = createMarinas(marinas);
 		ArrayList<Boat> boatObjs = createBoats(boats);
 		ArrayList<Person> peopleObjs = createPeople(people);
 		
+	
+	
+		//create relationships
+//		Boaty McBoatface captained by Peter Jackson
+		boatObjs.get(2).setCaptain(peopleObjs.get(6));
+//		Boaty McBoatface owned by Emma Watson
+		boatObjs.get(2).setOwners(peopleObjs.get(8));
+//		Boaty McBoatface crewed by Adele Ginger
+		boatObjs.get(2).setCrew(peopleObjs.get(7));
+//		Boaty McBoatface crewed by Avril Lavigne
+		boatObjs.get(2).setCrew(peopleObjs.get(9));
+//		Boaty McBoatface crewed by Brian Johnson
+		boatObjs.get(2).setCrew(peopleObjs.get(4));
+//		Santa Maria crewed by Peter Jackson
+		boatObjs.get(0).setCrew(peopleObjs.get(6));
+//		Mary’s dream owned by Mary Jones
+		boatObjs.get(1).setOwners(peopleObjs.get(1));
+//		Aquaholic Captained by Linus Torvalds
+		boatObjs.get(3).setCaptain(peopleObjs.get(5));
+//		Aquaholic owned by Bill Knightley
+		boatObjs.get(3).setOwners(peopleObjs.get(3));
+//		Aquaholic owned by David Green
+		boatObjs.get(3).setOwners(peopleObjs.get(0));
+		
+		
+		
+//		Aquaholic moored at Venice
+		marinaObjs.get(1).addBoat(boatObjs.get(3));
+//		Mary’s dream moored at Monaco
+		marinaObjs.get(0).addBoat(boatObjs.get(1));
+//		Boaty McBoatface moored at Monaco
+		marinaObjs.get(0).addBoat(boatObjs.get(2));
+//		Santa Maria moored at Venice
+		marinaObjs.get(1).addBoat(boatObjs.get(0));
+
+		
+		//Questions
+		
+		//oldest person at each port
+		int year = 3000;
+		for (Marina m:marinaObjs){
+			for (Boat b:m.getBoats()){
+				for (Person p:b.getAllcrew()){
+					String[] age = p.getDob().split("/");
+					if (Integer.parseInt(age[2])<year){
+						year = Integer.parseInt(age[2]);
+						
+					}
+				}
+			}
+			
+		}
+		//sysout
+		
+		//no. of boats at each port
+		
+		
+		//move a boat
+		
+		
+		//deliver a map of all boats in a marina
+		
+		
+		//report all people in a harbour ordered by boat.
+		
+		
+		
+		
 	}
 
+	
+	
+	
+	
+	
 	private static ArrayList<Marina> createMarinas(ArrayList<String> marinas) {
 		ArrayList<Marina> marinaObjs = new ArrayList<Marina>();
 		for (int i = 1; i<marinas.size()-2;i=i+3){
